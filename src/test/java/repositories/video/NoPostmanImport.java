531035bc67533dcf434fa101798bf0ae137853b7
marcosbelfastdev <marcosbelfastdev@gmail.com>
2025-08-20 15:47:33 -0300

    Adds NoPostmanImport featured in YouTube.

---
+++ src/test/java/repositories/video/NoPostmanImport.java
@@ -0,0 +1,57 @@
+package repositories.video;
+
+import br.com.erbium.core.Endpoint;
+import br.com.erbium.core.Workspace;
+import br.com.erbium.core.enums.Method;
+import br.com.erbium.core.enums.RequestType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class NoPostmanImport {
+
+    Workspace workspace;
+
+    @BeforeEach
+    public void setup() {
+        workspace = new Workspace();
+        workspace
+                .addCollection("My Fake Store Without Postman Collection")
+
+                .addEndpoint("Login", RequestType.JSON)
+                .setHost("{{host}}")
+                .setUrl("{{host}}/auth/login")
+                .setMethod(Method.POST)
+                .addHeader("Authorization", "{{access-token}}")
+                .getJsonRequest()
+                .setBody("""
+                        {
+                            username:"{{userName}}",
+                            password:"{{userPassword}}"
+                        }
+                        """).backToEndpoint()
+                .qrset("{{token}}", "$.token").backToCollection()
+
+
+                .addEndpoint("Get products", RequestType.JSON)
+                .addHeader("Authorization", "Bearer {{token}}")
+                .setHost("{{host}}")
+                .setUrl("{{host}}/products")
+                .setMethod(Method.GET);
+    }
+
+    @Test
+    public void test() {
+        workspace
+                .c$("My Fake Store Without Postman Collection")
+
+                .set("host", "https://fakestoreapi.com")
+                .set("{{userName}}", "mor_2314")
+                .set("userPassword", "83r5^_")
+                .set("access-token", "eoiwoiert349805u39084534")
+
+                .e$("Login").select()
+                .e$("Get products").select()
+
+                .selectedEndpoints(Endpoint::submit);
+    }
+}
