12c67784c1d5cb672d5afb0a400f34bc8db918ab
marcosbelfastdev <marcosbelfastdev@gmail.com>
2025-08-22 11:45:32 -0300

    Adds PostmanHelloWorld featured in YouTube.

--- src/test/java/repositories/video/PostmanHelloWorld.java
+++ src/test/java/repositories/video/PostmanHelloWorld.java
@@ -1,48 +1,41 @@
 package repositories.video;
 
+import admin.common.factories.MasterFakeStoreFactory;
 import admin.common.scripts.responses.CheckStatusCode;
 import admin.fakeStore.scripts.responses.SchemaValidation;
 import br.com.erbium.core.Endpoint;
-import br.com.erbium.core.LogItem;
-import br.com.erbium.core.LogType;
 import br.com.erbium.core.Workspace;
-import customization.utils.JwtTokenGenerator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import repositories.common.fakeStore.FakeStoreSchemas;
 
-import java.time.Duration;
-
 import static admin.common.factories.MasterFakeStoreFactory.*;
-import static admin.common.factories.MasterFakeStoreFactory.FAKESTORE_GET_CATEGORIES;
-import static admin.common.factories.MasterFakeStoreFactory.FAKESTORE_GET_PRODUCTS;
-import static admin.common.factories.MasterFakeStoreFactory.FAKESTORE_LOGIN;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PostmanHelloWorld {
 
     Workspace workspace;
 
-
     @BeforeEach
     public void setup() {
-        workspace = new Workspace()
 
+        workspace = new Workspace();
+        workspace
                 .addCollection(FAKESTORE_COLLECTION)
-                .importPostManCollection(FAKESTORE_COLLECTION_UID, FAKESTORE_POSTMAN_API_KEY, Duration.ofDays(365))
+                .importPostManCollection(FAKESTORE_COLLECTION_UID, FAKESTORE_POSTMAN_API_KEY)
 
-                .e$(FAKESTORE_LOGIN).qrset("token", "$.token").backToCollection()
-                .e$(FAKESTORE_GET_CATEGORIES).qrset("productCategory", "$.[-1]").backToCollection()
-                .e$(FAKESTORE_GET_PRODUCTS_BY_CATEGORY).qrset("productId", "$[-1]id").backToCollection()
-                .e$(FAKESTORE_GET_SINGLE_PRODUCT).backToCollection()
+                .e$(FAKESTORE_LOGIN).qrset("token", "$.token").select()
+                .e$(FAKESTORE_GET_CATEGORIES).qrset("productCategory", "$.[-1]").select()
+                .e$(FAKESTORE_GET_PRODUCTS_BY_CATEGORY).qrset("productId", "$.[-1]id").select()
+                .e$(FAKESTORE_GET_SINGLE_PRODUCT).select()
 
                 .selectedEndpoints(ep -> {
                     String schema = FakeStoreSchemas.get(ep);
-                    ep.addResponseScript("Check Status Code", new CheckStatusCode(ep))
-                      .addResponseScript("Check schema", new SchemaValidation().setSchema(schema, ep));
-                })
-                .workspaceContext();
+                    ep
+                            .addResponseScript("Check Status Code", new CheckStatusCode(ep))
+                            .addResponseScript("Check schema", new SchemaValidation().setSchema(schema, ep));
+                });
     }
 
     @Test
@@ -55,15 +48,10 @@ public class PostmanHelloWorld {
                 .set("userName", "mor_2314")
                 .set("userPassword", "83r5^_")
 
-                .e$(FAKESTORE_LOGIN).select()
-                .e$(FAKESTORE_GET_CATEGORIES).select()
-                .e$(FAKESTORE_GET_PRODUCTS_BY_CATEGORY).select()
-                .e$(FAKESTORE_GET_SINGLE_PRODUCT).select()
-
                 .selectedEndpoints(ep -> {
                     ep.submit();
                     assertTrue(ep.getResponseScript(CheckStatusCode.class).isStatusCodeAnyOf(200, 201), "Unexpected status code");
-                    assertTrue(ep.getResponseScript(SchemaValidation.class).isValidSchema(), "Invalid schema");
+                    assertTrue(ep. getResponseScript(SchemaValidation.class).isValidSchema(), "Invalid  schema");
                 });
     }
 }
